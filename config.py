5cd8b3722c416a318b1add47486978f8a5bf5069
20220698 <a.raudvee@student.tue.nl>
2023-06-07 21:23:40 +0200

    Alex final changes in the code 21:23

--- config.py
+++
@@ -1,5 +0,0 @@
-host = '127.0.0.1'
-user_name = 'root'
-port = 3306 
-password = 'Fatt_Ass100%'
-db_name = 'tweeter_db'
--- imports.py
+++
@@ -1,5 +0,0 @@
-import pymysql
-import json
-import sqlite3
-import pandas as pd
-import seaborn as sns 
